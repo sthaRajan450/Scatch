451bb504cfa6323b14a8a84bedeb207c8f3d05ff
sthaRaj9841 <thaneshworshrestha499@gmail.com>
2025-05-24 17:29:27 +0545

    development and production setup

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const ownersRouter=require('./routes/ownersRouter')
-const usersRouter=require('./routes/usersRouter')
-const productsRouter=require('./routes/productsRouter')
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
 
 const db = require("./config/mongoose-connection");
 
@@ -14,8 +14,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
-app.get("/owners", ownersRouter);
-app.get("/users", usersRouter);
-app.get("/products", productsRouter);
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000);
